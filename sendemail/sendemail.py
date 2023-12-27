a72e4ce98a8ef09e20c82febbdf8895df476269d
Greg Michnikov <greg.michnikov@gmail.com>
2023-12-27 13:36:23 -0500

    Add multiple people to to and bcc of email

--- sendemail/sendemail.py
+++ sendemail/sendemail.py
@@ -9,21 +9,23 @@ import os
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 
-from credentials import MY_EMAIL, APP_PASSWORD, RECEIVER_EMAIL
+from credentials import MY_EMAIL, APP_PASSWORD, RECEIVER_EMAILS, BCC_EMAILS
 
 # Email credentials
 sender_email = MY_EMAIL
 sender_password = APP_PASSWORD
-receiver_email = RECEIVER_EMAIL
+receiver_emails = RECEIVER_EMAILS
+bcc_emails = BCC_EMAILS
 
 # Email content
-subject = "Test Email from Python"
-body = "This is a test email sent from a Python script!"
+subject = "Hello the coof"
+body = "This email was sent from a Python script!"
 
 # Setting up the MIME
 message = MIMEMultipart()
 message["From"] = sender_email
-message["To"] = receiver_email
+message["To"] = ", ".join(receiver_emails)  # Join all 'to' email addresses
+message["Bcc"] = ", ".join(bcc_emails)  # Join all 'bcc' email addresses
 message["Subject"] = subject
 message.attach(MIMEText(body, "plain"))
 
@@ -32,7 +34,7 @@ try:
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()  # Secure the connection
         server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.sendmail(sender_email, receiver_emails + bcc_emails, message.as_string())
     print("Email sent successfully")
 except Exception as e:
     print(f"Error sending email: {e}")
